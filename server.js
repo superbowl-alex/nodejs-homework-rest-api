a4af3b1ee464a298526eb297c0320019355fb355
Oleksandr <superbowl@ukr.net>
2022-12-21 21:00:54 +0200

    Database connection implemented

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
+const mongoose = require("mongoose");
+
 const app = require("./src/app");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
+const DB_HOST = process.env.DB_HOST;
 
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`);
-});
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
