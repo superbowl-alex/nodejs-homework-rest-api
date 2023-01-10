669395e1ffdfa3de5624e5175380d8bc1ced1901
Oleksandr <superbowl@ukr.net>
2023-01-10 21:42:06 +0200

    Adds pagination

--- src/controllers/contacts/getAll.js
+++ src/controllers/contacts/getAll.js
@@ -2,7 +2,16 @@ const { Contact } = require("../../models/contact");
 
 const getAll = async (req, res) => {
   const { _id: owner } = req.user;
-  const contacts = await Contact.find({ owner }).populate("owner", "email");
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (page - 1) * limit;
+  const contacts = await Contact.find(
+    { owner },
+    {},
+    {
+      skip,
+      limit,
+    }
+  ).populate("owner", "email");
   res.status(200).json({ contacts });
 };
 
