01c41750f472257ac10137cc0028a6e956ad82ca
Oleksandr <superbowl@ukr.net>
2023-01-13 17:31:42 +0200

    Adds sort by Favorite field

--- src/controllers/contacts/getAll.js
+++ src/controllers/contacts/getAll.js
@@ -2,29 +2,27 @@ const { Contact } = require("../../models/contact");
 
 const getAll = async (req, res) => {
   const { _id: owner } = req.user;
-  const isFavorite = req.params.favorite;
-  // console.log(req.params);
-  // console.log(isFavorite);
+  const isFavorite = req.query.favorite;
   const { page = 1, limit = 20 } = req.query;
   const skip = (page - 1) * limit;
   const contacts =
-    // isFavorite !== undefined
-    //   ? await Contact.find(
-    //       { owner, favorite: { isFavorite } },
-    //       {},
-    //       {
-    //         skip,
-    //         limit,
-    //       }
-    //     ).populate("owner", "email")
-    await Contact.find(
-      { owner },
-      {},
-      {
-        skip,
-        limit,
-      }
-    ).populate("owner", "email");
+    isFavorite !== undefined
+      ? await Contact.find(
+          { owner, favorite: isFavorite },
+          {},
+          {
+            skip,
+            limit,
+          }
+        ).populate("owner", "email")
+      : await Contact.find(
+          { owner },
+          {},
+          {
+            skip,
+            limit,
+          }
+        ).populate("owner", "email");
   res.status(200).json({ contacts });
 };
 
